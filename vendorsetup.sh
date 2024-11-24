b1cc87956d978003396359b6e03c299a4b698d3e
RealShinzo <46410718+RealShinzo@users.noreply.github.com>
2024-11-24 11:45:39 +0100

    Update vendorsetup.sh
    swap
    omni to twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_emerald-user
-add_lunch_combo omni_emerald-userdebug
-add_lunch_combo omni_emerald-eng
+add_lunch_combo twrp_emerald-user
+add_lunch_combo twrp_emerald-userdebug
+add_lunch_combo twrp_emerald-eng
